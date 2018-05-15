3a468e934dd1a0fa83e1903af5cdaeb31799c2af
Raimon Esteve <raimon@nan-tic.com>
2018-05-15 21:39:46 +0200

    Allow extract report translations From trytond changeset-4ae75d974a78 issue5185

--- __init__.py
+++ __init__.py
@@ -2,14 +2,16 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import action
 from . import company
+from . import translation
 
 
 def register():
     Pool.register(
+        action.ActionReport,
         company.Company,
         module='html_report', type_='model')
-    # Pool.register(
-    #     module='html_report', type_='wizard')
-    # Pool.register(
-    #     module='html_report', type_='report')
+    Pool.register(
+        translation.ReportTranslationSet,
+        module='html_report', type_='wizard')
---
+++ translation.py
@@ -0,0 +1,14 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.pool import PoolMeta
+
+__all__ = ['ReportTranslationSet']
+
+
+class ReportTranslationSet:
+    __metaclass__ = PoolMeta
+    __name__ = "ir.translation.set"
+
+    def extract_report_jinja(self, content):
+        # TODO
+        return []
