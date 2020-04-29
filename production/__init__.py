236f696198304c01f1682ca6568b32ca15711ea7
Àngel Àlvarez <angel@nan-tic.com>
2020-04-29 07:52:20 +0200

    Finish production report

--- production/__init__.py
+++ production/__init__.py
@@ -1,6 +1,7 @@
 from trytond.pool import Pool
-
+from . import production
 
 def register(module):
     Pool.register(
+        production.Production,
         module=module, type_='model', depends=['production'])
