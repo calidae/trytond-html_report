a990e36bb97a5b1dfb06640cad2c4d0d68dc1581
Àngel Àlvarez <angel@nan-tic.com>
2020-06-01 10:49:19 +0200

    make header per report and fix some issues

--- invoice/invoice.py
+++ invoice/invoice.py
@@ -1,25 +1,19 @@
 from trytond.model import fields
 from trytond.pool import PoolMeta
-
+from collections import OrderedDict
 
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
-    sorted_lines = fields.Function(fields.One2Many('account.invoice.line',
-        'line', 'Sorted Lines'), 'get_sorted_lines')
     sorted_keys = fields.Function(fields.Char('Sorted Key'),
         'get_sorted_keys')
-
-    def get_sorted_lines(self, name):
-        lines = [x for x in self.lines]
-        lines.sort(key=lambda k: k.sort_key, reverse=True)
-        return [x.id for x in lines]
-
+       
     def get_sorted_keys(self, name):
+        
         lines = []
-        for x in self.sorted_lines:
+        for x in self.lines:
             if x.sort_key in lines:
                 continue
-            lines.append(x.sort_key)        
+            lines.append(x.sort_key)
         return lines
 
 
@@ -29,6 +23,7 @@ class InvoiceLine(metaclass=PoolMeta):
     sort_key = fields.Function(fields.Char('Sorted Key'),
         'get_sorted_key')
 
+    
     def get_sorted_key(self, name):
         key = []
         for move in self.stock_moves:
