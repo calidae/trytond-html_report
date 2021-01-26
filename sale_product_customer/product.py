d6dbbdb2aa5e00fb30b4687d3551b03642d7b4a7
MarinaNico <marina.nicolau@nan-tic.com>
2021-01-26 15:49:16 +0100

    Pyflakes fixes

--- sale_product_customer/product.py
+++ sale_product_customer/product.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta, Pool
+from trytond.pool import PoolMeta
 
 
 class ProductCustomer(metaclass=PoolMeta):
